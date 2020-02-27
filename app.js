8d1f635948fb1f38654f8704f98afc5d715bb232
sshankar <sshankar@cpaglobal.com>
2020-02-27 21:07:07 +1100

    apollo graphQl first cut

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+const {ApolloServer, gql} = require('apollo-server-express');
+const fs = require('fs');
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const userRoutes = require('./routes/user.routes');
 const app = express();
 
-const MONGO_URI = "mongodb://sri:"+ process.env.MONGO_URI +"@cluster0-shard-00-00-m6hr7.mongodb.net:27017,cluster0-shard-00-01-m6hr7.mongodb.net:27017,cluster0-shard-00-02-m6hr7.mongodb.net:27017/UserMgmt?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+const mongo_pwd = process.env.MONGO_URI || 'sri';
+const MONGO_URI = "mongodb://sri:" + mongo_pwd + "@cluster0-shard-00-00-m6hr7.mongodb.net:27017,cluster0-shard-00-01-m6hr7.mongodb.net:27017,cluster0-shard-00-02-m6hr7.mongodb.net:27017/UserMgmt?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+
+const typeDefs = gql(fs.readFileSync('./graphql/schema.graphql', {encoding: 'utf8'}));
+const resolvers = require('./graphql/resolvers');
+const apolloServer = new ApolloServer({typeDefs, resolvers});
+apolloServer.applyMiddleware({app, path: '/graphql'});
 
 app.use(express.json());
+
 app.use('/api/users', userRoutes);
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true })
@@ -17,4 +26,3 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true })
             .catch((err)=>{
                 console.log(err);
             });
-
