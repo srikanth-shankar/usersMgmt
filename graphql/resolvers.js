cbf1922a2003191b3aaed87ec82e459509913d7a
sshankar <sshankar@cpaglobal.com>
2020-02-27 23:04:59 +1100

    graphql complete

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,22 @@
+const User = require('../models/user.model');
+
 const Query = {
-    userMgmt: () => "user manamegement"
+    userMgmt: () => "user manamegement",
+    getUsers: () => User.find(),
+    getUserByName: (root, args) => User.findOne({name: args.name})
 }
 
-module.exports = {Query};
+const Mutation = {
+    createUser:  async (root, {name, email, pwd, isAdminRole}) => {
+        const user = new User({name, email, pwd, isAdminRole});
+        const newUser = await user.save();
+        return newUser.id;
+    },
+
+    deleteById: async (root, {id}) => {
+        result = await User.deleteOne({_id: id});
+        return !!result ? 'user deleted' : 'some prob occured';
+    }
+}
+
+module.exports = {Query, Mutation};
