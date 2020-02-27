c81badef2b77b0f9beabc1b856316a8f2a87dcd2
srikanth-shankar <srikanth.shankar2@gmail.com>
2020-02-27 17:09:53 +1100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const userRoutes = require('./routes/user.routes');
 const app = express();
 
-const MONGO_URI = "mongodb://sri:sri@cluster0-shard-00-00-m6hr7.mongodb.net:27017,cluster0-shard-00-01-m6hr7.mongodb.net:27017,cluster0-shard-00-02-m6hr7.mongodb.net:27017/UserMgmt?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+const MONGO_URI = "mongodb://sri:"+ process.env.MONGO_URI +"@cluster0-shard-00-00-m6hr7.mongodb.net:27017,cluster0-shard-00-01-m6hr7.mongodb.net:27017,cluster0-shard-00-02-m6hr7.mongodb.net:27017/UserMgmt?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
